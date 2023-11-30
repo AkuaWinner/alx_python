e0211390dc13778687db884afc4705e6d29f2e86
Maudie <maud10b@gmail.com>
2023-11-30 22:22:43 +0000

    python

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,24 +1,13 @@
-#import random
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
 
-#number = random.randint(-10, 10)
+print(f"{number} is", end=" ")
 
-#print("The number:", number)
-
-#if number > 0:
-#    print("is positive")
-#elif number == 0:
-#    print("is zero")
-#else:
-    #print("is negative")
-
-
-numbers = [-4, 0, -3, -10, 10, -5, 6, 7, 5]
-
-for number in numbers:
-    print(number, end=" ")
-    if number > 0:
-        print("is positive")
-    elif number == 0:
-        print("is zero")
-    else:
-        print("is negative")
+if number > 0:
+    print("positive")
+elif number == 0:
+    print("zero")
+else:
+    print("negative")
---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,5 @@
+import random
+
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+print(last_digit)
