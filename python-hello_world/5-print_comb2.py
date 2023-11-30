aaaed6ab58257bf639ff684a0942961ae4098862
Maudie <maud10b@gmail.com>
2023-11-30 22:32:32 +0000

    python

---
+++ python-hello_world/5-print_comb2.py
@@ -0,0 +1,10 @@
+# Print numbers from 0 to 99 separated by comma and space, with two digits
+
+# Print numbers from 0 to 99 with two digits
+for i in range(100):
+    print(f"{i:02d}", end=", ")    # Print each number with two digits and separated by comma and space
+print()  
+print("/n")
+
+
+
