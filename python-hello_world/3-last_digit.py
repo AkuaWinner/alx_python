693debb39ddc8d3e1af5d58ee1d0619ddfb57fd5
Maudie <maud10b@gmail.com>
2023-12-28 20:39:02 +0000

    functions

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,5 +1,10 @@
 import random
 
-number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-print(last_digit)
+number = random.randint(-10000, 10000) 
+num =  number[:1]
+response = "Last digit of " +str(number)+ "is" +num
+print (response)
+
+
+
+
