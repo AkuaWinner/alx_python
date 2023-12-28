75e536907dd4b375cf8ec68f79c8e3f6a485cd41
Maudie <maud10b@gmail.com>
2023-12-28 20:58:56 +0000

    functions

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,12 @@
+if n <= 0:
+    return []
+elif n == 1:
+        return [0]
+elif n == 2:
+            return [0, 1]
+else:
+    sequence = [0, 1]
+while len(sequence) < n:
+    next_number = sequence[-1] + sequence[-2]
+    sequence.append(next_number)
+return sequence
